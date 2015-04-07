12bf0e606e842c967ce633b54a418c08b7f89669
Alex Dean <alex@snowplowanalytics.com>
2015-04-07 10:42:10 +0100

    EmrEtlRunner: now allowing nil for config.yml's bootstrap field (closes #1575)

--- 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/contracts.rb
+++ 3-enrich/emr-etl-runner/lib/snowplow-emr-etl-runner/contracts.rb
@@ -82,7 +82,7 @@ module Snowplow
         :placement => Maybe[String],
         :ec2_subnet_id => Maybe[String],
         :ec2_key_name => String,
-        :bootstrap => ArrayOf[String],
+        :bootstrap => Maybe[ArrayOf[String]],
         :software => ({
           :hbase => Maybe[String],
           :lingual => Maybe[String]
